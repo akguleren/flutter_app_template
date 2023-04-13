a38d831163b81073872c25868393b46e54b7e818
akguleren <akguleren53@gmail.com>
2023-04-13 15:26:52 +0300

    initial commit

---
+++ lib/core/constants/app/app_constants.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class ApplicationConstants {
+  static const TEST_MODE = true;
+  static const LANG_ASSET_PATH = 'assets/languages';
+  static const EMAIL_REGIEX = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}\$';
+  static const LANGUAGE = 'language';
+  static const ON_BOARD_ID = 'onBoardContainer';
+  static const LOGIN_BUTTON_ID = 'loginButton';
+  static const SCAFFOLD_ID = 'scaffold';
+}
